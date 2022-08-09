072916c7c231636cc90cb0f327b62522502ab0d4
clairhardt98 <88024433+clairhardt98@users.noreply.github.com>
2022-08-10 01:54:49 +0900

    220810

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PlayerAnim
+{
+    public AnimationClip idle;
+    public AnimationClip runF;
+    public AnimationClip runB;
+    public AnimationClip runL;
+    public AnimationClip runR;
+
+}
 public class PlayerCtrl : MonoBehaviour
 {
     private float h = 0.0f;
     private float v = 0.0f;
     private float r = 0.0f;
 
-    [SerializeField] private Transform tr;
+    private Transform tr;
     public float moveSpeed = 10.0f;
     public float rotSpeed = 80.0f;
+
+    public PlayerAnim playerAnim;//인스펙터 뷰에 표시할 애니메이션 클래스 변수
+    [HideInInspector]public Animation anim;// 컴포넌트를 저장하기 위한 변수
+
     void Start()
     {
         tr = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
+        anim.clip = playerAnim.idle;
+        anim.Play();
     }
 
     // Update is called once per frame
@@ -31,5 +48,26 @@ public class PlayerCtrl : MonoBehaviour
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
 
         tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+
+        if(v >= 0.1f)
+        {
+            anim.CrossFade(playerAnim.runF.name, 0.3f);
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade(playerAnim.runB.name, 0.3f);
+        }
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade(playerAnim.runR.name, 0.3f);
+        }
+        else if (h <= -0.1f)
+        {
+            anim.CrossFade(playerAnim.runL.name, 0.3f);
+        }
+        else
+        {
+            anim.CrossFade(playerAnim.idle.name, 0.3f);
+        }
     }
 }
