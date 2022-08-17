9c01e3050e0cb0b677afe4d26dfc349c33b55207
clairhardt98 <88024433+clairhardt98@users.noreply.github.com>
2022-08-17 16:45:06 +0900

    220817

--- Assets/02.Scripts/Enemy/EnemyFire.cs
+++ Assets/02.Scripts/Enemy/EnemyFire.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class EnemyFire : MonoBehaviour
 {
@@ -10,25 +11,41 @@ public class EnemyFire : MonoBehaviour
     private Transform enemyTr;
 
     private readonly int hashFire = Animator.StringToHash("Fire");
+    private readonly int hashReload = Animator.StringToHash("Reload");
 
     private float nextFire = 0.0f;
     private readonly float fireRate = 0.1f;//사격 속도
     private readonly float damping = 10.0f;//회전 속도
 
+    private readonly float reloadTime = 2.0f;
+    private readonly int maxBullet = 10;
+    private int currBullet = 10;
+    private bool isReload = false;
+
+    private WaitForSeconds wsReload;
+
     public bool isFire = false;
     public AudioClip fireSfx;
+    public AudioClip reloadSfx;
+
+    public GameObject Bullet;
+    public Transform firePos;
+
+    public MeshRenderer muzzleFlash;
     void Start()
     {
         playerTr = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>();
         enemyTr = GetComponent<Transform>();
         animator = GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
+
+        wsReload = new WaitForSeconds(reloadTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isFire)
+        if (!isReload && isFire)
         {
             if (Time.time >= nextFire)
             {
@@ -45,5 +62,43 @@ public class EnemyFire : MonoBehaviour
     {
         animator.SetTrigger(hashFire);
         audio.PlayOneShot(fireSfx, 1.0f);
+        StartCoroutine(ShowMuzzleFlash());
+
+        GameObject _bullet = Instantiate(Bullet, firePos.position, firePos.rotation);
+        Destroy(_bullet, 3.0f);
+        
+        isReload = (--currBullet % maxBullet == 0);
+        if (isReload)
+        {
+            StartCoroutine(Reloading());
+        }
+    }
+
+    IEnumerator Reloading()
+    {
+        muzzleFlash.enabled = false;
+        animator.SetTrigger(hashReload);
+        audio.PlayOneShot(reloadSfx,1.0f);
+
+        yield return wsReload;
+
+        currBullet = maxBullet;
+        isReload = false;
+    }
+
+    IEnumerator ShowMuzzleFlash()
+    {
+        muzzleFlash.enabled = true;
+
+        Quaternion rot = Quaternion.Euler(Vector3.forward * Random.Range(0, 360));
+        muzzleFlash.transform.localRotation = rot;
+        muzzleFlash.transform.localScale = Vector3.one * Random.Range(1.0f, 2.0f);
+
+        Vector2 offset = new Vector2(Random.Range(0, 2), Random.Range(0, 2)) * 0.5f;
+        
+        muzzleFlash.material.SetTextureOffset("_MainTex",offset);
+
+        yield return new WaitForSeconds(Random.Range(0.05f, 0.2f));
+        muzzleFlash.enabled = false;
     }
 }
