a6c6a7c75336d7a2c343531b44162aab4fc81d16
clairhardt98 <88024433+clairhardt98@users.noreply.github.com>
2022-09-19 03:42:12 +0900

    220919

--- Assets/02.Scripts/Enemy/EnemyDamage.cs
+++ Assets/02.Scripts/Enemy/EnemyDamage.cs
@@ -3,20 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using PlasticPipe.PlasticProtocol.Messages;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyDamage : MonoBehaviour
 {
     private const string bulletTag = "BULLET";
 
     private float hp = 100.0f;
+    private float initHp = 100.0f;
 
     private GameObject bloodEffect;
+
+    public GameObject hpBarPrefab;
+
+    public Vector3 hpBarOffset = new Vector3(0, 2.2f, 0);
+
+    private Canvas uiCanvas;
+
+    private Image hpBarImage;
     // Start is called before the first frame update
     void Start()
     {
+        //enemyAI = GetComponent<EnemyAI>();
         bloodEffect = Resources.Load<GameObject>("BulletImpactFleshBigEffect");
+        SetHpBar();
     }
 
+    void SetHpBar()
+    {
+        uiCanvas = GameObject.Find("UI Canvas").GetComponent<Canvas>();
+        GameObject hpBar = Instantiate<GameObject>(hpBarPrefab, uiCanvas.transform);
+        hpBarImage = hpBar.GetComponentsInChildren<Image>()[1];
+
+        var _hpBar = hpBar.GetComponent<EnemyHpBar>();
+        _hpBar.targetTr = GetComponent<Transform>();
+        _hpBar.offset = hpBarOffset;
+    }
     void OnCollisionEnter(Collision coll)
     {
         if (coll.gameObject.CompareTag(bulletTag))
@@ -25,10 +47,13 @@ public class EnemyDamage : MonoBehaviour
             //Destroy(coll.gameObject);
             coll.gameObject.SetActive(false);
             hp -= coll.gameObject.GetComponent<BulletCtrl>().damage;
+
+            hpBarImage.fillAmount = hp / initHp;
             
             if (hp <= 0.0f)
             {
                 GetComponent<EnemyAI>().state = EnemyAI.State.DIE;
+                hpBarImage.GetComponentsInParent<Image>()[1].color = Color.clear;
             }
         }
     }
